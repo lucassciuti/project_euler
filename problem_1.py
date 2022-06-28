c62c57ed96751393658ff5b40ed7775e8e57cd1a
lucas_sciuti <lfsciuti@gmail.com>
2022-06-28 20:11:52 -0300

    adding problem_1 solution

--- problem_1.py
+++ problem_1.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def is_multiple_of_3(i):
     return i % 3 == 0
 
@@ -8,17 +6,19 @@ def is_multiple_of_5(i):
     return i % 5 == 0
 
 
-def get_multipliers(number):
+def get_multipliers(m, n, number):
     multiples = [] 
     
     for i in range(number):
-        if is_multiple_of_3(i) or is_multiple_of_5(i):
+        if i % m == 0 or i % n == 0:
             multiples.append(i)
     
     return sum(multiples)
 
 
 if __name__ == "__main__":
-    i = 1000
-    print(get_multipliers(i))
+    m = 3
+    n = 5
+    number = 1000
+    print(get_multipliers(m, n, number))
 
